a0f35167798484b6308d35a2bd5f66dc4f464e9d
Aljon Preza <aljonpreza@yahoo.com>
2015-09-22 10:14:02 -1000

    Working on Campaign

--- campaign.js
+++ campaign.js
@@ -1,3 +1,22 @@
 /**
  * Created by aljonp on 9/22/15.
  */
+var sampleData = campaignContributions.slice(0, 10);
+
+
+function groupCampaign(data) {
+  return _.groupBy(data, 'Party');
+}
+
+function currencyToNum(currencyString) {
+  return Number(currencyString.replace(/[^0-9\.]+/g,""));
+}
+
+function addTotal(memo, cont) {
+  return memo + currencyToNum(cont["Amount"]);
+}
+
+
+
+
+console.log(meep(campaignContributions));
